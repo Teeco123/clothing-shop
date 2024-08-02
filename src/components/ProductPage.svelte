9a013a9cab700e0b1e797f1b9326d4b006438504
Kacper <kacper.gajko1@gmail.com>
2024-08-02 11:51:20 +0200

    centering product page

--- src/components/ProductPage.svelte
+++ src/components/ProductPage.svelte
@@ -9,108 +9,116 @@
 </script>
 
 <div class="wrapper">
-	{#each products as product}
-		<div class="product-info">
-			<div class="product-name">{product.name}</div>
-			<div class="product-price">{product.price} ZŁ</div>
-			<div class="sizes">
-				<div class="sizes-text">SIZE</div>
-				<div class="sizes-buttons">
-					{#each product.sizes as size}
-						<div class="size-button">
-							<input
-								type="radio"
-								id="size-input-{size.size}"
-								name="size"
-								value={size.size}
-								bind:group={selectedSize}
-							/>
-							<label for="size-input-{size.size}">
-								{size.size}
-							</label>
-						</div>
-					{/each}
+	<div class="product-page">
+		{#each products as product}
+			<div class="product-info">
+				<div class="product-name">{product.name}</div>
+				<div class="product-price">{product.price} ZŁ</div>
+				<div class="sizes">
+					<div class="sizes-text">SIZE</div>
+					<div class="sizes-buttons">
+						{#each product.sizes as size}
+							<div class="size-button">
+								<input
+									type="radio"
+									id="size-input-{size.size}"
+									name="size"
+									value={size.size}
+									bind:group={selectedSize}
+								/>
+								<label for="size-input-{size.size}">
+									{size.size}
+								</label>
+							</div>
+						{/each}
+					</div>
 				</div>
+				<button class="add-to-cart">Add to cart</button>
 			</div>
-			<button class="add-to-cart">Add to cart</button>
-		</div>
-		<div class="product-images"></div>
-	{/each}
+			<div class="product-images"></div>
+		{/each}
+	</div>
 </div>
 
 <style lang="scss">
 	.wrapper {
 		display: flex;
+		justify-content: center;
 		height: calc(100vh - 180px);
-		padding: 5% 10%;
+
 		color: white;
-		.product-info {
-			width: 55%;
-			margin: 32px;
-			.product-name {
-				font-size: 3.5vw;
-				margin: 16px;
-			}
-			.product-price {
-				font-size: 2vw;
-				margin: 16px;
-			}
-			.sizes {
-				margin: 42px 16px 16px;
-				.sizes-buttons {
-					display: grid;
-					grid-template-columns: repeat(6, 72px);
-					grid-gap: 12px;
-					margin-top: 16px;
-					.size-button {
-						display: flex;
-						width: 100%;
-						height: 36px;
-						label {
+		.product-page {
+			display: flex;
+			flex-direction: row;
+			max-width: 1500px;
+			.product-info {
+				width: 55%;
+				margin: 32px;
+				.product-name {
+					font-size: 3.5vw;
+					margin: 16px;
+				}
+				.product-price {
+					font-size: 2vw;
+					margin: 16px;
+				}
+				.sizes {
+					margin: 42px 16px 16px;
+					.sizes-buttons {
+						display: grid;
+						grid-template-columns: repeat(6, 72px);
+						grid-gap: 12px;
+						margin-top: 16px;
+						.size-button {
 							display: flex;
-							justify-content: center;
-							align-items: center;
 							width: 100%;
-							height: 100%;
-							border: 1px white solid;
-							transition: 0.5s;
-							&:hover {
-								cursor: pointer;
-								background-color: rgb(25, 25, 25);
+							height: 36px;
+							label {
+								display: flex;
+								justify-content: center;
+								align-items: center;
+								width: 100%;
+								height: 100%;
+								border: 1px white solid;
+								transition: 0.5s;
+								&:hover {
+									cursor: pointer;
+									background-color: rgb(40, 40, 40);
+								}
 							}
-						}
-						input[type='radio'] {
-							width: 0;
-							height: 0;
-							opacity: 0;
-							margin: 0;
-						}
-						input[type='radio']:checked + label {
-							background-color: white;
-							color: black;
+							input[type='radio'] {
+								width: 0;
+								height: 0;
+								opacity: 0;
+								margin: 0;
+							}
+							input[type='radio']:checked + label {
+								background-color: white;
+								color: black;
+							}
 						}
 					}
 				}
-			}
-			.add-to-cart {
-				margin: 16px;
-				border: 1px white solid;
-				height: 36px;
-				width: calc((6 * 72px) + (5 * 12px));
-				color: black;
-				background-color: white;
-				font-weight: bold;
-				font-size: 14px;
-				transition: 0.3s;
-				&:hover {
-					cursor: pointer;
-					color: white;
-					background-color: black;
+				.add-to-cart {
+					margin: 16px;
+					border: 1px white solid;
+					height: 36px;
+					width: calc((6 * 72px) + (5 * 12px));
+					color: black;
+					background-color: white;
+					font-weight: bold;
+					font-size: 14px;
+					transition: 0.3s;
+					&:hover {
+						cursor: pointer;
+						color: white;
+						background-color: black;
+					}
 				}
 			}
-		}
-		.product-images {
-			width: 45%;
+			.product-images {
+				width: 45%;
+			}
 		}
 	}
 </style>
