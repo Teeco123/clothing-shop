25d327d0e94848a936688fd052ff2d2038c5a134
Kacper <kacper.gajko1@gmail.com>
2024-08-07 20:48:28 +0200

    template of item with static info

--- src/components/Cart.svelte
+++ src/components/Cart.svelte
@@ -6,7 +6,23 @@
 			<div class="quantity-label">QUANTITY</div>
 			<div class="total-label">TOTAL</div>
 		</div>
-		<div class="cart-items"></div>
+		<div class="cart-items">
+			<div class="item">
+				<div class="item-img">
+					<img
+						src="https://xgsegeuhgevlcrridgyk.supabase.co/storage/v1/object/public/product-photos/Gowno%20bluza/Gowno%20bluza0.webp"
+						alt="bluza"
+					/>
+				</div>
+				<div class="item-info">
+					<div class="item-name">GOWNO BLUZA</div>
+					<div class="item-price">45ZŁ</div>
+					<div class="item-size">SIZE: M</div>
+				</div>
+				<div class="item-quantity">2</div>
+				<div class="item-total">90ZŁ</div>
+			</div>
+		</div>
 	</div>
 </div>
 
@@ -31,21 +47,53 @@
 				flex-direction: row;
 				.product-label {
 					width: 60%;
-					text-align: center;
 				}
 				.quantity-label {
 					width: 20%;
-					text-align: center;
 				}
 				.total-label {
 					width: 20%;
-					text-align: center;
 				}
 			}
 			.cart-items {
 				border-top: 1px white solid;
 				width: calc(100vw - 64px);
 				max-width: 1500px;
+				.item {
+					display: flex;
+					flex-direction: row;
+					align-items: center;
+					margin: 24px 0px;
+					.item-img {
+						width: 10%;
+						img {
+							width: 100%;
+							object-fit: cover;
+						}
+					}
+					.item-info {
+						margin-left: 26px;
+						width: calc(50% - 26px);
+						.item-name {
+							margin-bottom: 26px;
+							font-weight: 600;
+							font-size: 18px;
+						}
+						.item-price {
+							margin-bottom: 12px;
+							font-weight: 500;
+						}
+						.item-size {
+							font-weight: 500;
+						}
+					}
+					.item-quantity {
+						width: 20%;
+					}
+					.item-total {
+						width: 20%;
+					}
+				}
 			}
 		}
 	}
